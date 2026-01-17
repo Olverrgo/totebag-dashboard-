fb85f0ed66027f6fbf4874788fcf9029f3ac7c3c
Olverrgo <olverrgo@gmail.com>
2026-01-17 08:19:19 -0500

    Change sidebar text to CMYK 0-17-40-0 = #FFD499 (peach cream)

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -559,7 +559,7 @@ const colors = {
   camel: '#C9A96E',
   espresso: '#4A3728',
   sidebarBg: '#FAABB3',
-  sidebarText: '#FF8C42',
+  sidebarText: '#FFD499',
   olive: '#6B7B5E',
   gold: '#B8954F',
   cotton: '#F8F4EF',
